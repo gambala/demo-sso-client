ec11667dc844b262398ee6c951bf2163fa8c3f4b
Gambala <gambala.rus@gmail.com>
2013-08-04 03:04:40 +0400

    Added some commented strings to initialization options

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,8 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :accounts, ENV['ACCOUNTS_API_ID'], ENV['ACCOUNTS_API_SECRET'],
 		client_options: {
-			site: ENV['ACCOUNTS_API_SITE']
+			site: ENV['ACCOUNTS_API_SITE'],
+			authorize_url: ENV['ACCOUNTS_API_AUTHORIZE_URL'],
+			access_token_url: ENV['ACCOUNTS_API_ACCESS_TOKEN_URL']
 		}
 end

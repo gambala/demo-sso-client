4ef5a27e832b192620ed62b2e23c72fca06460b6
Gambala <gambala.rus@gmail.com>
2013-08-04 02:52:43 +0400

    Changed initialization options

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,8 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
 	provider :accounts, ENV['ACCOUNTS_API_ID'], ENV['ACCOUNTS_API_SECRET'],
 		client_options: {
-			site: 'http://localhost:3001',
-			authorize_url: '/auth',
-			access_token_url: '/asd'
+			site: ENV['ACCOUNTS_API_SITE']
 		}
 end
